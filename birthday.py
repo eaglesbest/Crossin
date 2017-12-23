e25285f761398152fd6d7a236a9c19f2ad938bce
fanyj <hnyjfan@163.com>
2017-12-23 20:14:54 +0800

    change birthday.py to print the last num smaller than 20171231

--- birthday.py
+++ birthday.py
@@ -6,26 +6,31 @@ month_1 = ['01','03','05','07','08','10','12']
 month_2 = ['04','06','09','11']
 month_3 = ['02']
 count = 0
+result_list = []
  
 for bir in permutations(num_list,8):
     num = ''.join(bir)
     year = num[0:4]
     month = num[4:6]
     day = num[6:8]
-    if month in month_1:
-        if int(day) > 32:
-            continue
-    elif month in month_2:
-        if int(day) > 31:
-            continue 
-    elif month in month_3:
-        if ((int(year) % 4 == 0) & (int(year) % 100 != 0)) | (int(year) % 400 == 0):
-            if int(day) > 30:
-                continue
-        else:
-            if int(day) > 29:
-                continue 
+    if int(year) > 2017:
+        break
     else:
-        continue
-    print num
-    break
+        if month in month_1:
+            if int(day) > 32:
+                continue
+        elif month in month_2:
+            if int(day) > 31:
+                continue 
+        elif month in month_3:
+            if ((int(year) % 4 == 0) & (int(year) % 100 != 0)) | (int(year) % 400 == 0):
+                if int(day) > 30:
+                    continue
+            else:
+                if int(day) > 29:
+                    continue 
+        else:
+            continue
+        result_list.append(num)
+print len(result_list)
+print result_list[-1]
